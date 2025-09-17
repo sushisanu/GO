4138ae9f4789ac86ffd512375a5b284a669f2bce
sushisanu <sushisanuk@kkumail.com>
2025-09-17 14:01:56 +0700

    commit

--- demo19_cast_and_check_type/index.go
+++ demo19_cast_and_check_type/index.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 )
 
 type shape interface {
@@ -30,13 +31,26 @@ func getArea(s shape) float64{
 }
 
 func showInfo(s shape){
-   
+  t := reflect.TypeOf(s).Name()
+  switch t {
+  case "rectangle":
+	r := s.(rectangle);
+	fmt.Printf("Rect  width: %f, height: %f\n", r.width, r.height)
+	break
+  case "circle":
+	c := s.(circle);
+	fmt.Printf("Circle radius: %f\n", c.radius)
+  }
+
 }
 
 func main() {
 	r := rectangle{width: 10, height: 10}
-	c := circle{radius: 10}	
+	c := circle{radius: 10}		
 
 	fmt.Printf("Rectangle: %f\n", getArea(r))
 	fmt.Printf("Circle: %f\n", getArea(c))
+
+	showInfo(r)
+	showInfo(c)
 }
